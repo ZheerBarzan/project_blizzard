87bb6ac2fe1e942d39874667416a15468634001c
zheer barzan <zheerbarzan85@outlook.com>
2024-04-09 18:34:58 +0300

    adding the json file

---
+++ project_blizzard/Views/WeatherView.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherView.swift
+//  project_blizzard
+//
+//  Created by zheer barzan on 9/4/24.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var weather: ResponseBody
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherView()
+}
