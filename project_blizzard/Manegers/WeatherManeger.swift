2d6b0617bc4ec5eb87a0817951a23d9ae5bc873b
zheer barzan <zheerbarzan85@outlook.com>
2024-04-09 18:14:47 +0300

    decodeing the json file

--- project_blizzard/Manegers/WeatherManeger.swift
+++ project_blizzard/Manegers/WeatherManeger.swift
@@ -10,7 +10,7 @@ import CoreLocation
 
 class WeatherManeger{
     
-    func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws{
+    func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws -> ResponseBody{
         
         guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\("32accf81cdf0a4a5e91727a4dd7e80f1")&units=metric") else { fatalError("URL is missing")}
         
@@ -21,7 +21,50 @@ class WeatherManeger{
         
         guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Invalid response")}
         
+       let decodedData =  try JSONDecoder().decode(ResponseBody.self, from: data)
+        
+        return decodedData
         
     }
     
+}
+
+struct ResponseBody: Decodable {
+    var coord: CoordinatesResponse
+    var weather: [WeatherResponse]
+    var main: MainResponse
+    var name: String
+    var wind: WindResponse
+
+    struct CoordinatesResponse: Decodable {
+        var lon: Double
+        var lat: Double
+    }
+
+    struct WeatherResponse: Decodable {
+        var id: Double
+        var main: String
+        var description: String
+        var icon: String
+    }
+
+    struct MainResponse: Decodable {
+        var temp: Double
+        var feels_like: Double
+        var temp_min: Double
+        var temp_max: Double
+        var pressure: Double
+        var humidity: Double
+    }
+    
+    struct WindResponse: Decodable {
+        var speed: Double
+        var deg: Double
+    }
+}
+
+extension ResponseBody.MainResponse {
+    var feelsLike: Double { return feels_like }
+    var tempMin: Double { return temp_min }
+    var tempMax: Double { return temp_max }
 }
