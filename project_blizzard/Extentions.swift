ed8e23fbcf09fafc2f624088e7c62a0e47168c68
zheer barzan <zheerbarzan85@outlook.com>
2024-04-12 15:46:45 +0300

    rounding the double data type

---
+++ project_blizzard/Extentions.swift
@@ -0,0 +1,14 @@
+//
+//  Extentions.swift
+//  project_blizzard
+//
+//  Created by zheer barzan on 12/4/24.
+//
+
+import Foundation
+
+extension Double {
+    var formatted: String {
+        return String(format: "%.0f", self)
+    }
+}
