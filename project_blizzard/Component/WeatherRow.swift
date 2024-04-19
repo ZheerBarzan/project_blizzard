4ffaacd2eb95a9364331a7d8334f01f1b74c4bff
zheer barzan <zheerbarzan85@outlook.com>
2024-04-19 18:33:57 +0300

    im Iron-man

---
+++ project_blizzard/Component/WeatherRow.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherRow.swift
+//  project_blizzard
+//
+//  Created by zheer barzan on 19/4/24.
+//
+
+import SwiftUI
+
+struct WeatherRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherRow()
+}
