149b920cdefa74bb4ba1ae4cf4d4a25c2bff57d4
zheer barzan <zheerbarzan85@outlook.com>
2024-04-19 17:38:38 +0300

    adding the tempreture and the location on the view of the application

--- project_blizzard/Views/WeatherView.swift
+++ project_blizzard/Views/WeatherView.swift
@@ -14,9 +14,37 @@ struct WeatherView: View {
         ZStack(alignment: .leading){
             VStack{
                 VStack(alignment: .leading, spacing: 5){
+                    Text(weather.name)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
                     
+                    Text("today,\(Date().formatted(.dateTime.month().day().hour().minute()))")
+                        .font(.title2)
+                        .foregroundColor(.white)
                 }.frame(maxWidth: .infinity, alignment: .leading)
+                Spacer()
                 
+                VStack{
+                    HStack{
+                        VStack(spacing: 10){
+                            Image(systemName: "sun.max").font(.system(size: 40))
+                            Text(weather.weather[0].main)
+                                .font(.title)
+                                .fontWeight(.light)
+                                .foregroundColor(.white)
+                            
+                        }.frame(width: 150, alignment: .leading)
+                        
+                        
+                        Text(weather.main.feelsLike.formatted()+"°")
+                            .font(.system(size: 60))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white).padding()
+                    }
+                    Spacer().frame(height: 80)
+                    
+                }.frame(maxWidth: .infinity)
             }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading).padding()
             
         }.edgesIgnoringSafeArea(.bottom).background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
