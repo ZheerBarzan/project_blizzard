ce2e912a971770d60810a617e13b6ec4e0723015
zheer barzan <zheerbarzan85@outlook.com>
2024-04-19 18:14:37 +0300

    adding the bottom section of the application to the weather view

--- project_blizzard/Views/WeatherView.swift
+++ project_blizzard/Views/WeatherView.swift
@@ -44,8 +44,28 @@ struct WeatherView: View {
                     }
                     Spacer().frame(height: 80)
                     
+                    AsyncImage(url: URL(string: "https://pngimg.com/uploads/city/city_PNG38.png")) { image in
+                        image.resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .scaledToFit()
+                            .frame(width: 300)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    
+                    Spacer()
+                    
+                    
                 }.frame(maxWidth: .infinity)
             }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading).padding()
+            
+            
+            VStack{
+                Spacer()
+                VStack(alignment: .leading, spacing: 20){
+                    
+                }.frame(maxWidth: .infinity, alignment: .leading).padding().padding(.bottom,20).foregroundColor(Color(hue: 0.656, saturation: 0.787, brightness: 0.354)).background(.white).cornerRadius( 20, corners: [.topLeft,.topRight])
+            }
             
         }.edgesIgnoringSafeArea(.bottom).background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
             .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
